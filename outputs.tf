aa9dc4c44842352c87d7d9815d6e0a799aada459
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:37 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "chaos_studio_capabilities" {
   description = "All chaos_studio_capability resources"
   value       = azurerm_chaos_studio_capability.chaos_studio_capabilities
 }
+output "chaos_studio_capabilities_capability_type" {
+  description = "List of capability_type values across all chaos_studio_capabilities"
+  value       = [for k, v in azurerm_chaos_studio_capability.chaos_studio_capabilities : v.capability_type]
+}
+output "chaos_studio_capabilities_chaos_studio_target_id" {
+  description = "List of chaos_studio_target_id values across all chaos_studio_capabilities"
+  value       = [for k, v in azurerm_chaos_studio_capability.chaos_studio_capabilities : v.chaos_studio_target_id]
+}
+output "chaos_studio_capabilities_urn" {
+  description = "List of urn values across all chaos_studio_capabilities"
+  value       = [for k, v in azurerm_chaos_studio_capability.chaos_studio_capabilities : v.urn]
+}
 
