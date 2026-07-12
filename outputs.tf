de32be5e46ead4f65bb542be43b8de93d9dd49ed
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:23 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "chaos_studio_capabilities" {
-  description = "All chaos_studio_capability resources"
-  value       = azurerm_chaos_studio_capability.chaos_studio_capabilities
-}
 output "chaos_studio_capabilities_capability_type" {
-  description = "List of capability_type values across all chaos_studio_capabilities"
-  value       = [for k, v in azurerm_chaos_studio_capability.chaos_studio_capabilities : v.capability_type]
+  description = "Map of capability_type values across all chaos_studio_capabilities, keyed the same as var.chaos_studio_capabilities"
+  value       = { for k, v in azurerm_chaos_studio_capability.chaos_studio_capabilities : k => v.capability_type }
 }
 output "chaos_studio_capabilities_chaos_studio_target_id" {
-  description = "List of chaos_studio_target_id values across all chaos_studio_capabilities"
-  value       = [for k, v in azurerm_chaos_studio_capability.chaos_studio_capabilities : v.chaos_studio_target_id]
+  description = "Map of chaos_studio_target_id values across all chaos_studio_capabilities, keyed the same as var.chaos_studio_capabilities"
+  value       = { for k, v in azurerm_chaos_studio_capability.chaos_studio_capabilities : k => v.chaos_studio_target_id }
 }
 output "chaos_studio_capabilities_urn" {
-  description = "List of urn values across all chaos_studio_capabilities"
-  value       = [for k, v in azurerm_chaos_studio_capability.chaos_studio_capabilities : v.urn]
+  description = "Map of urn values across all chaos_studio_capabilities, keyed the same as var.chaos_studio_capabilities"
+  value       = { for k, v in azurerm_chaos_studio_capability.chaos_studio_capabilities : k => v.urn }
 }
 
